dbcbe609d815e1917c52607ca98d0f9b758abad8
s21482 <s21482@pjwstk.edu.pl>
2023-01-29 16:26:22 +0100

    Dodaj pliki projektów.

---
+++ LinqTutorials/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task14();
+            foreach (var task in t)
+            {
+                Console.WriteLine(task);
+            }
+            
+            Console.WriteLine(LinqTasks.Task13(new int[] { 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1 }));
+        }
+    }
+}
